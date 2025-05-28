596cdeb54b400ca896a27c6c33b23d44c086492b
Yasashvee Karthi <yasashvee@gmail.com>
2025-05-28 18:00:41 -0500

    Delete SRAMSimulator.java

--- SRAMSimulator.java
+++
@@ -1,7 +0,0 @@
-public class SRAMSimulator {
-    private boolean[] memory;  // true = 1, false = 0
-
-    public SRAMSimulator(int size) {
-        memory = new boolean[size];  // starts with all 0s
-    }
-}
